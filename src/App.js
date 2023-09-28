028f38cc403c870d9bfdb95a6ce6318c31a6ee17
qcluu <lcao29@gmail.com>
2023-09-28 15:23:46 -0400

    Co-authored-by: Artemis <IcariusFowl@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const quotes = [
+  {
+    text: "BOINKE 1",
+    author: "BOINKIE"
+  },
+  {
+    text: "BOINKE 2",
+    author: "BOINKIE"
+  },
+  {
+    text: "BOINKE 3",
+    author: "BOINKIE"
+  },
+  {
+    text: "BOINKE 4",
+    author: "BOINKIE"
+  },
+  {
+    text: "BOINKE 5",
+    author: "BOINKIE"
+  },
+]
 
 function App() {
+
+  const [quote, setQuote] = useState(quotes[Math.floor(Math.random() * quotes.length)]);
+  const getRandomQuote = () => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div id="quote-box">
+        <p id='text'>{quote.text}</p>
+        <p id='author'>{quote.author}</p>
+        <button id='new-quote' onClick={getRandomQuote}>
+          New Quote
+        </button>
+        <a id='tweet-quote' href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${quote.text}" - ${quote.author}`} target='_top'>
+          TWEET
         </a>
-      </header>
+      </div>
     </div>
   );
 }
